49e3b6c610a1fcd42606581668b07ee4305f88c9
NeoDragon <mcvickerstephen@gmail.com>
2020-08-11 13:42:10 +0100

    Updates

---
+++ precache-manifest.4600406974079ed206b47e4db602de21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3498c8d15ec73886a252184128c160",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e0c3bcb0f4b2f015242f",
+    "url": "/portfolio/static/css/main.19b367b1.chunk.css"
+  },
+  {
+    "revision": "674532f6c188d61d749b",
+    "url": "/portfolio/static/js/2.70fd9b8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.70fd9b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c3bcb0f4b2f015242f",
+    "url": "/portfolio/static/js/main.e33e3983.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a6cff2794d7583215c941cb5490614b8",
+    "url": "/portfolio/static/media/CPMaskLogo2.a6cff279.png"
+  },
+  {
+    "revision": "9d0bee130d6c49b3fe660f97e35103fa",
+    "url": "/portfolio/static/media/Me2.9d0bee13.jpg"
+  }
+]);
