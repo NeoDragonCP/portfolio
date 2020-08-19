abd358c2ed515088f50f153ff4315a48d0356b30
NeoDragon <mcvickerstephen@gmail.com>
2020-08-19 23:18:44 +0100

    Updates

--- precache-manifest.8d541e2a4af3f0b2c95790b19bc803dc.js
+++ precache-manifest.8d541e2a4af3f0b2c95790b19bc803dc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7d374fa988e011b4ed3f81933999e84",
+    "revision": "3ee7d7c33932570c32ca6e7c1ff5e819",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "2a5e53cd7a2c6b461a5c",
+    "revision": "ca9c8577e68d3f0fc14a",
     "url": "/portfolio/static/css/main.98f04e80.chunk.css"
   },
   {
-    "revision": "cf2f363c04eb7b1d74c5",
-    "url": "/portfolio/static/js/2.e7a1d240.chunk.js"
+    "revision": "46995c407ab1aa7dc8af",
+    "url": "/portfolio/static/js/2.9a90b007.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.e7a1d240.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.9a90b007.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2a5e53cd7a2c6b461a5c",
-    "url": "/portfolio/static/js/main.971dd4fd.chunk.js"
+    "revision": "ca9c8577e68d3f0fc14a",
+    "url": "/portfolio/static/js/main.92e030be.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b65e91ed2a13fd055d77fb79e33059d2.js"
+  "/portfolio/precache-manifest.8d541e2a4af3f0b2c95790b19bc803dc.js"
 );
 
 self.addEventListener('message', (event) => {
